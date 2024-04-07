ef04f4e016eb3b19b22473c44f3e9e3965515f03
Taufiq <taufiq9.2004@gmail.com>
2024-04-07 09:42:12 +0530

    bug fix

--- src/components/UploadProfilePic.js
+++ src/components/UploadProfilePic.js
@@ -8,12 +8,8 @@ import { useSelector } from "react-redux";
 
 const UploadProfilePic = () => {
   const [img, setImg] = useState("");
-  const [instaId, setInstaId] = useState(null);
   const [imgPath, setImgPath] = useState(null);
-  const [userId, setUserId] = useState(null);
   const [userName, setUserName] = useState(null);
-  const [uploadImg, setUploadImg] = useState('choose your Img');
-  const [uploadName, setUploadName] = useState('');
 
   const userNaam = useSelector((store) => store.user.userName);
 
@@ -36,7 +32,7 @@ const UploadProfilePic = () => {
     updateProfile(auth.currentUser, {
       displayName: userName,
     })
-      .then((r) => setUploadName('Username has been updated !'))
+      .then((r) => console.log(r))
       .catch((e) => console.log(e));
 
 //       const handleInstaId =()=>{
@@ -48,40 +44,37 @@ const UploadProfilePic = () => {
 //       }
 
   return (
-    <div className="">
     <div className="lg:mx-40 lg:my-24 mx-3 my-10">
-      <h1 className="p-1 pb-11 font-semibold text-xl">User's Profile</h1>
-      <span className="font-semibold p-1">Username:</span>{userNaam}
+      <div className="font-semibold text-xl pb-14">User's Profile</div>
+      <span>Username: </span>{userNaam}
       <div className="my-12">
-        <label className="text-lg font-semibold">Change your Username</label>
+        <label className="text-lg font-semibold px-1">Change your Username</label>
         <div className=" my-2">
           <input
-            className="p-2 border-[1px]  border-black rounded-l-md lg:w-96 w-60"
+            className="p-2 border-[1px]  border-black lg:rounded-l-md w-96 mb-3 lg:pb-0"
             type="text"
             placeholder="Enter you user name"
-            onChange={(e) => {setUserName(e.target.value)}}
+            onChange={(e) => setUserName(e.target.value)}
           />
           <button
             onClick={handleUserName}
-            className="bg-black text-white p-2 border-[1px] border-black rounded-r-md"
+            className="bg-black text-white p-2 border-[1px] border-black lg:rounded-r-md"
           >
-            User Name
+            Set Username
           </button>
         </div>
-        <p>{uploadName}</p>
       </div>
       <label className="py-5 my-2 text-lg font-semibold">
         Upload your profile image
       </label>
-      <div className="lg:h-[100px]">
-        <div className="w-[100px] h-[100px] 50px  bg-sky-200 absolute flex items-center justify-center text-gray-600 text-sm p-2 my-2 text-center rounded-l-md">
-          <p className="">{uploadImg}</p>
+      <div className="h-[100px]">
+        <div className="w-[100px] h-[100px] bg-sky-200 absolute flex items-center justify-center text-gray-600 text-sm p-2 my-2 text-center rounded-l-md">
+          <p className="">Choose a image</p>
         </div>
         <input
           className=" bg-sky-200 rounded-l-md w-[100px] h-[100px] text-center align-middle cursor-pointer p-1 opacity-0 my-2"
           type="file"
-          onChange={(e) => {setImg(e.target.files[0])
-            setUploadImg('Click upload')}}
+          onChange={(e) => setImg(e.target.files[0])}
         />
         <button
           className="bg-black w-[100px] h-[100px] border-0 border-black rounded-r-md text-white my-2"
@@ -110,7 +103,6 @@ const UploadProfilePic = () => {
       </div>
 
       </div>
-    </div>
     </div>
   );
 };
